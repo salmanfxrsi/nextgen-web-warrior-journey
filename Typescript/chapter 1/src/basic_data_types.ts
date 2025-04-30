f9108fa9a67266c9939c4413f4e78c0f50b8dc0d
salmanfxrsi <salmanxprivate@gmail.com>
2025-04-30 21:57:41 +0600

    feat: practice TypeScript basic data types including string, number, boolean, array, and tuple

---
+++ Typescript/chapter 1/src/basic_data_types.ts
@@ -0,0 +1,24 @@
+// Basic Data Types
+
+// String
+let firstName : string = "Salman";
+// Number
+let roll : number = 1;
+// Boolean
+let isAdmin : boolean = true;
+// Undefined
+let age : undefined = undefined;
+// Null
+let address : null = null;
+
+
+let d : number;
+d = 123;
+
+// Array
+let friends : string[] = ["rachel", "monica"];
+let rollList : number[] = [1, 2, 3, 4, 5, 6];
+
+// Tuple --> Array --> Order --> Type Of Values
+let coordinates : [number, number] = [1, 5];
+let ageName : [number, string] = [25, "Salman"]; 
