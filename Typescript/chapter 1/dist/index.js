cc57ff829fd32f9ee4ed430e28a4551db4b1756d
salmanfxrsi <salmanxprivate@gmail.com>
2025-04-30 21:11:28 +0600

    feat: write my first TypeScript code

---
+++ Typescript/chapter 1/dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+var text = "I wanna be a greatest developer in the world";
+console.log(text);
---
+++ Typescript/chapter 1/src/index.ts
@@ -0,0 +1,2 @@
+let text : string = "I wanna be a greatest developer in the world";
+console.log(text);
