3bb9e35222565e8402f9e4f2688ad0d7b99c00ba
salmanfxrsi <salmanxprivate@gmail.com>
2025-05-04 12:25:04 +0600

    chore: practiced TypeScript generics with arrays, objects, and tuples using custom generic types

---
+++ Typescript/chapter 2/generics.ts
@@ -0,0 +1,42 @@
+{
+// Generics
+type GenericArray<T> = Array<T>;
+
+// const name: string[] = ["Alex", "Green", "Michel"];
+const name: GenericArray<string> = ["Alex", "Green", "Michel"];
+
+// const age: number[] = [20, 34, 53, 78, 89];
+const age: GenericArray<number> = [20, 34, 53, 78, 89];
+
+// const isAdmin: boolean[] = [false, true, true, false];
+const isAdmin: GenericArray<boolean> = [false, true, true, false];
+
+const user : GenericArray<{name: string, age: number}> = [
+    {
+        name: "Farsi",
+        age: 100,
+    },
+    {
+        name: "Farsi",
+        age: 100,
+    },
+    {
+        name: "Farsi",
+        age: 100,
+    },
+    {
+        name: "Farsi",
+        age: 100,
+    },
+    {
+        name: "Farsi",
+        age: 100,
+    },
+]
+
+// Generic Tuple 
+type Student<X, Y> = [X, Y]; 
+const student : Student<number, {name: string, age: number}> = [1, {name: "Alex", age: 53}];
+
+//
+}
