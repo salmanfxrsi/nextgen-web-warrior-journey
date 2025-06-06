81f514b6aeeb7b098d59756cbd566db75d7b5609
salmanfxrsi <salmanxprivate@gmail.com>
2025-06-06 17:12:14 +0600

    chore: learn named exports, aliasing, and index exports in Node.js

--- Express & Mongoose/learning-node/file-2.js
+++ Express & Mongoose/learning-node/file-2.js
@@ -1,3 +1,5 @@
 const { a, add, b } = require("./file-1")
-console.log(a);
-console.log(add(10, 20));
+const { a : a3, add : add3, b : b3 } = require("./file-3")
+console.log(a3);
+console.log(add3(10, 20, 30));
+console.log(b3);
---
+++ Express & Mongoose/learning-node/main.js
@@ -0,0 +1,4 @@
+const { add, subtract } = require("./utils/index");
+
+console.log(add(10, 20));
+console.log(subtract(10, 20));
