abc2f44d5bc85f97f01d0a291f17d59b98980fbf
salmanfxrsi <salmanxprivate@gmail.com>
2025-06-06 15:35:29 +0600

    chore: explore CommonJS module system

---
+++ Express & Mongoose/learning-node/file-1.js
@@ -0,0 +1,11 @@
+const a = 10;
+const b = 20;
+
+const add = (param1, param2) => param1 + param2;
+
+module.exports = {
+    a,
+    b,
+    add,
+};
+console.log(module);
---
+++ Express & Mongoose/learning-node/file-2.js
@@ -0,0 +1,3 @@
+const { a, add, b } = require("./file-1")
+console.log(a);
+console.log(add(10, 20));
